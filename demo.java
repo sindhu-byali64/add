793f98d148341c4ce380af486164bf94b73864fc
unknown <sindhutbyali6@gmail.com>
2025-04-23 14:44:49 +0530

    save

---
+++ demo.java
@@ -0,0 +1,12 @@
+ public class demo {
+    public static void main(String[] args) {
+        double num1 = 10.5;
+        double num2 = 5;
+
+        double addition= num1 + num2;
+        double subtraction = num1 - num2;
+
+        System.out.println("Addition: " + addition);
+        System.out.println("Subtraction: " + subtraction);
+    }
+}
